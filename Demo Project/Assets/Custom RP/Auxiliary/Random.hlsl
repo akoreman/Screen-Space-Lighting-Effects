a0d39ea5cb7c473853a9fd344c3cc1abd2b47447
Alice <a.koreman@outlook.com>
2023-01-31 21:30:24 +0100

    added link to noise

--- Demo Project/Assets/Custom RP/Auxiliary/Random.hlsl
+++ Demo Project/Assets/Custom RP/Auxiliary/Random.hlsl
@@ -1,6 +1,7 @@
 #ifndef CUSTOM_RANDOM_INCLUDED
 #define CUSTOM_RANDOM_INCLUDED
 
+// Based on https://bitinn.github.io/ScriptableRenderPipeline/ShaderGraph/Random-Range-Node/
 float RandomRange_float(float2 Seed, float Min, float Max)
 {
     float randomno =  frac(sin(dot(Seed, float2(12.9898, 78.233)))*43758.5453);
